8af18d13be0f313f8bfe3bd375f94ef978f9133e
WillYip716 <william.yip.716@gmail.com>
2020-11-16 17:11:01 -0800

    safeway and frys price scraper

--- getprices.js
+++ getprices.js
@@ -1,25 +1,68 @@
 const puppeteer = require('puppeteer');
 const $ = require('cheerio');
-const url = 'https://www.frysfood.com/search?query=honey%20nut%20cheerios&searchType=suggestions&fulfillment=all';
 
-(async () => {
+async function findItemPrices(item,store) {
     // prepare for headless chrome
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-
+    let url;
     // set user agent (override the default headless User Agent)
     await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
 
+    if(store === "frys"){
+        url = "https://www.frysfood.com/search?query=" + escape(item) + "&searchType=suggestions&fulfillment=all";
+    }
+    else if(store === "safeway"){
+        url = "https://www.safeway.com/shop/search-results.html?q="+ escape(item);
+    }
     // go to Google home page
     await page.goto(url);
 
     // get the User Agent on the context of Puppeteer
     const content = await page.content();
-    $('div.ProductCard', content).each((index,element) => {
-        console.log(index);
-    });
+
+    if(store === "frys"){
+        console.log("contacting frys");
+        $('div.ProductCard', content).each((index,element) => {
+            let productName = $(element).find("h3");
+            console.log($(productName[0]).text());
+    
+            let price = $(element).find("data.kds-Price");
+            if(price[0]){
+                console.log(price[0].attribs.value);
+            }
+            else{
+                console.log("no found value");
+            }
+    
+            let size = $(element).find("span[data-qa='cart-page-item-sizing']");
+            console.log($(size[0]).text());
+        });
+    }
+    else if(store === "safeway"){
+        console.log("contacting safeway");
+        $('div.card-body', content).each((index,element) => {
+            let productName = $(element).find("h3");
+            console.log($(productName[0]).text());
+    
+            let price = $(element).find("span.product-price");
+            if(price[0]){
+                console.log($(price[0]).text());
+            }
+            else{
+                console.log("no found value");
+            }
+    
+            let size = $(element).find("span.product-price-qty");
+            console.log($(size[0]).text());
+        });
+    }
+    
     // If everything correct then no 'HeadlessChrome' sub string on userAgent
     
-
     await browser.close();
-})();
+}
+
+
+findItemPrices("honey nut cheerios", "frys");
+findItemPrices("honey nut cheerios", "safeway");
